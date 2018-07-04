b4e82f70f7240f3d86d976e87a5d2dbd1b07de20
bezeal <mwesterink@gmail.com>
2018-07-04 22:53:48 +0200

    Initial commit

---
+++ src/consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, object>
+            {
+                { "group.id", "sample-consumer" },
+                { "bootstrap.servers", "localhost:9092" },
+                { "enable.auto.commit", "false"},
+                { "auto.offset.reset", "earliest"}
+            };
+
+            using (var consumer = new Consumer<Null, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+            {                
+                consumer.Subscribe(new string[]{"hello-topic"});
+
+                consumer.OnMessage += (_, msg) => 
+                {
+                    Console.WriteLine($"Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
+                    consumer.CommitAsync(msg);
+                };
+
+                Console.WriteLine("Waiting for messages...");
+                while (true)
+                {
+                    consumer.Poll(100);
+                }
+        }
+        }
+    }
+}
---
+++ src/producer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", "localhost:9092" }
+            };
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                string text = null;
+
+                while (text != "exit")
+                {
+                    System.Console.Write("Text: ");
+                    text = Console.ReadLine();                                                                                                                                                              
+                    var result = producer.ProduceAsync("hello-topic", null, text).Result;
+                    System.Console.WriteLine($"Error: {result.Error}");
+                }
+
+                producer.Flush(100);
+            }
+        }
+    }
+}
